cd8ee4f2db5d6098175285b233d153f8f0d8e63c
Paul <paul.hollander001@gmail.com>
2024-07-05 13:57:25 +0200

    cmake

--- env_support/cmake/custom.cmake
+++ env_support/cmake/custom.cmake
@@ -83,11 +83,11 @@ install(
   RENAME "lv_conf.h"
   OPTIONAL)
 
-configure_file("${LVGL_ROOT_DIR}/lvgl.pc.in" lvgl.pc @ONLY)
+#configure_file("${LVGL_ROOT_DIR}/lvgl.pc.in" lvgl.pc @ONLY)
 
-install(
-  FILES "${CMAKE_BINARY_DIR}/lvgl.pc"
-  DESTINATION "${LIB_INSTALL_DIR}/pkgconfig/")
+#install(
+#  FILES "${CMAKE_BINARY_DIR}/lvgl.pc"
+#  DESTINATION "${LIB_INSTALL_DIR}/pkgconfig/")
 
 set_target_properties(
   lvgl
